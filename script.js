190ae344aa95762042332675192dfb6be0f50346
wowster123 <78980307+wowster123@users.noreply.github.com>
2022-06-30 14:32:04 -0500

    About Me

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('hometown').innerHTML = "Memphis, Tennesse";
+document.getElementById('favorites').innerHTML = "Spreding Smiles and Love";
+document.getElementById('nickname').innerHTML = "NaeNae";
+
+var img = document.createElement("img");
+
+img.src = "https://scontent-atl3-1.xx.fbcdn.net/v/t1.6435-9/69022086_10212149004839819_4100757623723261952_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=jrWu6RW1d-MAX_nie0a&_nc_ht=scontent-atl3-1.xx&oh=00_AT_tITixTrrNFO7W3Vc9qiyNKmPF0gWfjuSBD6bUEKAIXA&oe=62E4E2AA";
+var src = document.getElementById("x");
+
+src.appendChild(img);
